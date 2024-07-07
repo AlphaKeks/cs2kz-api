5e8a1f8fd4bbca13b9c2e3aa1cd9171cb4d36299
AlphaKeks <alphakeks@dawn.sh>
2024-07-07 18:47:53 +0200

    tooling: split nix stuff into more files and rework Justfile

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-//! Copyright (C) 2024  AlphaKeks <alphakeks@dawn.sh>
+//! CS2KZ API - the core infrastructure for CS2KZ.
+//! Copyright (C) 2024  AlphaKeks <alphakeks@dawn>
 //!
-//! CS2KZ API.
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
 //!
-//! This is free software: you can redistribute it and/or modify it under
-//! the terms of the GNU General Public License as published by the Free
-//! Software Foundation, either version 3 of the License, or (at your option)
-//! any later version.
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//! GNU General Public License for more details.
 //!
-//! This is distributed in the hope that it will be useful, but WITHOUT ANY
-//! WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-//! FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-//! details.
-//!
-//! You should have received a copy of the GNU General Public License along with
-//! this repository. If not, see <https://www.gnu.org/licenses/>.
+//! You should have received a copy of the GNU General Public License
+//! along with this program. If not, see https://www.gnu.org/licenses.
 
 use std::backtrace::Backtrace;
 use std::panic;
