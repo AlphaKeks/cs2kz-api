f617440c00e952383068c0e70de80719b5ee2728
AlphaKeks <alphakeks@dawn.sh>
2024-07-07 19:59:15 +0200

    refactor: attach license headers to `cs2kz`

--- crates/cs2kz/src/lib.rs
+++ crates/cs2kz/src/lib.rs
@@ -1,4 +1,8 @@
-//! Library for working with CS2KZ.
+//! The CS2KZ "standard library".
+//!
+//! This crate contains a set of core types and functions related to CS2KZ.
+//! It is primarly used by the API, but may be published and used in other
+//! projects in the future.
 
 mod identifier;
 
