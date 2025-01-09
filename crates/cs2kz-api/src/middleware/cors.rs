1e40d2a5a908827a769d4a406e729c621388ea2f
AlphaKeks <alphakeks@dawn.sh>
2025-01-09 05:13:25 +0100

    cors attempt 1

---
+++ crates/cs2kz-api/src/middleware/cors.rs
@@ -0,0 +1,37 @@
+use http::{HeaderValue, Method, Uri, header, request};
+use tower_http::cors::{AllowCredentials, AllowHeaders, AllowMethods, AllowOrigin, CorsLayer};
+
+const KNOWN_HOSTS: &[&str] = &["https://dashboard.cs2kz.org"];
+const LOCAL_HOSTS: &[&str] = &["0.0.0.0", "127.0.0.1", "::", "::1", "localhost"];
+
+pub fn layer() -> CorsLayer {
+    CorsLayer::new()
+        .allow_credentials(AllowCredentials::predicate(allow_credentials))
+        .allow_headers(AllowHeaders::mirror_request())
+        .allow_methods(AllowMethods::any())
+        .allow_origin(AllowOrigin::predicate(allow_origin))
+        .expose_headers([header::COOKIE])
+}
+
+fn allow_credentials(_header: &HeaderValue, request: &request::Parts) -> bool {
+    matches!(request.method, Method::POST | Method::PUT | Method::PATCH | Method::DELETE)
+}
+
+fn allow_origin(header: &HeaderValue, request: &request::Parts) -> bool {
+    if !allow_credentials(header, request) {
+        // request isn't sensitive
+        return true;
+    }
+
+    let Ok(origin) = header.to_str() else {
+        return false;
+    };
+
+    if cfg!(feature = "production") {
+        return KNOWN_HOSTS.contains(&origin);
+    }
+
+    origin
+        .parse::<Uri>()
+        .is_ok_and(|uri| uri.host().is_some_and(|host| LOCAL_HOSTS.contains(&host)))
+}
